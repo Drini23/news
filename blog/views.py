df2865e6580b9062955ea6a37551eef6fb3fb1d2
Drini23 <122325981+Drini23@users.noreply.github.com>
2024-09-13 23:47:26 +0200

    first

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,11 @@ import pytz
 from django.shortcuts import render
 from football.api import football_api
 
-
 def home(request):
     return render(request, 'blog/home.html')
 
-
 def fetch_team_details(team_id, headers):
-    api_url = f'http://api.football-data.org/v2/teams/{team_id}'
+    api_url = f'http://api.football-data.org/v4/teams/{team_id}'
     response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
         return response.json()
@@ -18,10 +16,9 @@ def fetch_team_details(team_id, headers):
         print(f"Failed to fetch team details for team ID {team_id}. Status code: {response.status_code}")
         return None
 
-
 def today_matches(request):
     today = date.today().isoformat()
-    api_url = f'http://api.football-data.org/v2/matches?dateFrom={today}&dateTo={today}'
+    api_url = f'http://api.football-data.org/v4/matches'
     headers = {'X-Auth-Token': football_api}
 
     response = requests.get(api_url, headers=headers)
@@ -37,11 +34,10 @@ def today_matches(request):
             home_team_details = fetch_team_details(home_team_id, headers)
             away_team_details = fetch_team_details(away_team_id, headers)
 
-            match['homeTeam']['crest'] = home_team_details.get('crestUrl',
-                                                               '/static/img/default_logo.png') if home_team_details else '/static/img/default_logo.png'
-            match['awayTeam']['crest'] = away_team_details.get('crestUrl',
-                                                               '/static/img/default_logo.png') if away_team_details else '/static/img/default_logo.png'
-
+            # You can still include team names or other details if needed
+            match['homeTeam']['name'] = home_team_details.get('name') if home_team_details else 'Unknown Home Team'
+            match['awayTeam']['name'] = away_team_details.get('name') if away_team_details else 'Unknown Away Team'
+            
             # Parse and format the match time
             utc_date = match['utcDate']
             match_time = datetime.fromisoformat(utc_date.replace('Z', '+00:00'))
@@ -53,5 +49,3 @@ def today_matches(request):
     else:
         error_message = f"Failed to fetch today's matches from the API. Status code: {response.status_code}"
         return render(request, 'blog/error.html', {'error': error_message})
-
-
