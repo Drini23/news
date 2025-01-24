602bff29ff566edb1cad1aa8a578dedf3aa1e1d8
Drini23 <122325981+Drini23@users.noreply.github.com>
2025-01-25 00:36:32 +0100

    false

--- football/settings.py
+++ football/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b%lm&fxx-0!nob4)edg$*du+bv@^ro3)wgv*qom28mv564jeth'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
