96880bfa0a8d700fc5a079c7ba416c8e50378120
drini12345 <drini12345>
2025-11-19 21:28:19 +0100

    database

--- football/settings.py
+++ football/settings.py
@@ -131,20 +131,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'football.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'football_al24',
-        'USER': 'root',
-        'PASSWORD': 'drini123',  # replace this
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
